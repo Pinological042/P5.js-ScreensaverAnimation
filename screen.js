7654b980bfe18e261e0e6a994ef28688154974fc
Pinological042 <moonfire042@gmail.com>
2019-08-02 10:35:50 +0545

    add

---
+++ screen.js
@@ -0,0 +1,18 @@
+
+function setup(){
+  	createCanvas(1280, 720);
+  	background(0,10,30);
+
+}
+function draw(){
+
+	noStroke();
+	fill(random(0,255),random(0,255),random(0,255));
+	circle(random(0,1280),random(0,720),random(0,random(10,40)));
+	//circle(random(0,1280),random(0,720),random(0,random(10,40)));
+
+	//circle(random(mouseX,mouseX + 50),random(mouseY,mouseY + 50),random(0,random(10,40)));
+
+
+
+}
