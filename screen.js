4ef9d285d184a684cb881e66c81e8482eada4235
Peter Karki <46894030+Pinological042@users.noreply.github.com>
2019-08-14 08:18:24 +0545

    update

--- screen.js
+++ screen.js
@@ -8,7 +8,7 @@ function draw(){
 
 	noStroke();
 	fill(random(0,255),random(0,255),random(0,255));
-	circle(random(0,1280),random(0,720),random(0,random(10,40)));
+	circle(mouseX,mouseY,random(0,random(10,40)));
 	//circle(random(0,1280),random(0,720),random(0,random(10,40)));
 
 	//circle(random(mouseX,mouseX + 50),random(mouseY,mouseY + 50),random(0,random(10,40)));
